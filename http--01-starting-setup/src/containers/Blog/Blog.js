5fadb4100276f922c9529fcd67f94a4278d57bdf
unknown <aseelaldallal@gmail.com>
2017-12-08 14:35:12 -0500

    complete

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import asyncComponent from '../../hoc/AsyncComponent';
+
+const AsyncNewPost = asyncComponent(()=> {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+
+    state = {
+        auth: true
+    }
 
     render () {
         return (
@@ -21,8 +30,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch> 
-                    <Route path="/new" exact component={NewPost}/>
+                    {this.state.auth? <Route path="/new" exact component={AsyncNewPost}/> : null }
                     <Route path="/posts" component={Posts}/>   
+                    <Route render={() => <h1> Not found </h1>}/>
+                    {/* <Redirect from="/" to="/posts" />  */}
+                    {/* <Route path="/" component={Posts}/>    */}
                 </Switch>
             </div>
         );
