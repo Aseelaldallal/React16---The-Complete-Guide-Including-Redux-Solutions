cb627d3eefab3eb7b5593530744e7372e5a158bc
unknown <aseelaldallal@gmail.com>
2017-12-11 15:12:50 -0500

    complete routing problem

--- routing-problem/src/containers/Course/Course.js
+++ routing-problem/src/containers/Course/Course.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+
+import './Course.css';
 
 class Course extends Component {
+
+    state = {
+        title: null,
+        id: null
+    }
+
+    componentDidMount() {
+        this.loadData(); 
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData = () => {
+        if(this.props.match.params.id != this.state.id) {
+            const query = new URLSearchParams(this.props.location.search);
+            this.setState({
+                title: query.get('title'),
+                id: this.props.match.params.id
+            });
+        }
+    }
+
     render () {
+        console.log(this.props);
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1 className="title">{this.state.title}</h1>
+                <p>You selected the Course with ID: {this.state.id} </p>
             </div>
         );
     }
 }
 
-export default Course;
+export default withRouter(Course);
