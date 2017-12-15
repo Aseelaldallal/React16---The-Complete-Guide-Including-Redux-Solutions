5baba39f6344e93cbfcfefaf192f06c2dba2e2f0
unknown <aseelaldallal@gmail.com>
2017-12-15 13:56:22 -0500

    implement add person

--- redux--assignment-2-problem/src/store/reducer.js
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -1,9 +1,33 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     persons: []
 }
 
 const reducer = (state = initialState, action) => {
-    return state;
+    switch(action) {
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), 
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return { persons: state.persons.concat(newPerson) };
+        case actionTypes.REMOVE_PERSON:
+            return;
+        default:
+            return state;
+    }
 }
 
 export default reducer;
+
+
+
+
+personAddedHandler = () => {
+
+    this.setState( ( prevState ) => {
+        return { persons: prevState.persons.concat(newPerson)}
+    } );
+}
