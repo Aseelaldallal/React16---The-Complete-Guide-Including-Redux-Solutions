8b13570d7ea28c9a9a92f0ce2ca2115861ff961f
unknown <aseelaldallal@gmail.com>
2017-12-15 13:43:16 -0500

    link reducer to store

--- redux--assignment-2-problem/src/index.js
+++ redux--assignment-2-problem/src/index.js
@@ -8,10 +8,12 @@ import ReactDOM from 'react-dom';
 // Redux
 import { createStore } from 'redux';
 import { Provider    } from 'react-redux'
+import reducer from './store/reducer';
 // CSS
 import './index.css';
 
-const store = createStore();
+
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
